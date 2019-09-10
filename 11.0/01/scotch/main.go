b4081e4e7ab4d32a6ce367f4d32f639bbb41b6b1
Mike Mulderink <mulderink1@gmail.com>
2019-09-10 15:06:41 -0700

    Finished 11.0 folder

---
+++ 11.0/01/scotch/main.go
@@ -0,0 +1,9 @@
+// Package scotch for the best scotch
+package scotch
+
+import "fmt"
+
+// Scotch returns a string with the best scotch
+func Scotch() string {
+	return fmt.Sprint(`Glenlivet 12 year`)
+}
